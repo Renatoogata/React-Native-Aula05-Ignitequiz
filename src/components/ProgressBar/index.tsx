95c4054e820c3179235c4db6e6bb903d70581584
Renato Ide Ogata <renatoogata95@gmail.com>
2023-05-02 18:39:27 -0300

    ProgressBar animation

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react';
 import { View } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
 import { styles } from './styles';
 
@@ -8,11 +10,24 @@ interface Props {
 }
 
 export function ProgressBar({ total, current }: Props) {
-  const percentage = Math.round((current / total) * 100);
+  //Math.round para não ter número quebrado
+  const percentage = Math.round((current / total) * 100); // pegando o total de perguntas pela pergunta q a pessoa esta respondendo no momento e multiplicando por 100 para ter o valor em porcentagem 
+
+  const sharedProgress = useSharedValue(percentage);
+
+  const styleAnimated = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress.value}%`
+    }
+  })
+
+  useEffect(() => {
+    sharedProgress.value = withTiming(percentage, { duration: 700 });
+  }, [current]) // current é a pergunta que o usuário está ou seja, sempre que o usuário passar para próxima pergunta o useEffect será disparado
 
   return (
     <View style={styles.track}>
-      <View style={[styles.progress, { width: `${percentage}%` }]} />
+      <Animated.View style={[styles.progress, styleAnimated]} />
     </View>
   );
 }
