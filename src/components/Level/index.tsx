7f06abcd3a464a7d9b33a5c243e0b955afba8580
Renato Ide Ogata <renatoogata95@gmail.com>
2023-05-02 17:37:27 -0300

    useSharedValue, useAnimatedStyle, Animated -> essentials for react native: reanimated

--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -1,4 +1,5 @@
 import { TouchableOpacity, TouchableOpacityProps, Text, View } from 'react-native';
+import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated'
 
 import { THEME } from '../../styles/theme';
 import { styles } from './styles';
@@ -16,14 +17,23 @@ type Props = TouchableOpacityProps & {
 }
 
 export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Props) {
+  const scale = useSharedValue(1); //criar variável para guardar conteúdo para usar nas animações
 
   const COLOR = TYPE_COLORS[type];
 
+  const animatedContainerStyle = useAnimatedStyle(() => { //useAnimatedStyle é uma estilização reativa ao useSharedValues
+    return { // Temos que usar o useAnimatedStyle para definir quais as propriedades a gente quer animar da estilização do componente
+      transform: [{ scale: scale.value }] // sacle.value -> é no value que está o conteúdo definido no useSharedValues
+    }
+  })
+
   return (
     <TouchableOpacity {...rest}>
-      <View style={
+      <Animated.View style={ // A gente só pode usar um componente animado em um componente que aceite o componente animado nesse acaso Animated.View
+        // Se eu utilizasse uma View normal do react native ela não ia aceitar as props de animção que eu fiz
         [
           styles.container,
+          animatedContainerStyle, // utilizando o animatedStyle
           { borderColor: COLOR, backgroundColor: isChecked ? COLOR : 'transparent' }
         ]
       }>
@@ -34,7 +44,7 @@ export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Prop
           ]}>
           {title}
         </Text>
-      </View>
+      </Animated.View>
     </TouchableOpacity>
   );
 }
