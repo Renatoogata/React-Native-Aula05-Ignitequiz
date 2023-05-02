abd56a3ee2bbac9860a9f20dff41f584979b1e8d
Renato Ide Ogata <renatoogata95@gmail.com>
2023-05-02 18:14:56 -0300

    color interpolate with reanimated

--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -1,9 +1,11 @@
+import { useEffect } from 'react';
 import { Pressable, PressableProps, Text } from 'react-native';
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
   withTiming,
-  Easing
+  Easing,
+  interpolateColor
 } from 'react-native-reanimated'
 
 import { THEME } from '../../styles/theme';
@@ -23,12 +25,28 @@ type Props = PressableProps & {
 
 export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Props) {
   const scale = useSharedValue(1); //criar variável para guardar conteúdo para usar nas animações
+  const checked = useSharedValue(1); // temo que criar uma propriedade que o reanimated vai conseguir etender(criamos os cheked para obter a informação do isCheked)
 
   const COLOR = TYPE_COLORS[type];
 
   const animatedContainerStyle = useAnimatedStyle(() => { //useAnimatedStyle é uma estilização reativa ao useSharedValues
     return { // Temos que usar o useAnimatedStyle para definir quais as propriedades a gente quer animar da estilização do componente
-      transform: [{ scale: scale.value }] // sacle.value -> é no value que está o conteúdo definido no useSharedValues
+      transform: [{ scale: scale.value }], // sacle.value -> é no value que está o conteúdo definido no useSharedValues
+      backgroundColor: interpolateColor(
+        checked.value,
+        [0, 1], // valores possíveis
+        ['transparent', COLOR] // valores para quando for 0 ou 1
+      )
+    }
+  })
+
+  const animatedTextStyle = useAnimatedStyle(() => {
+    return {
+      color: interpolateColor(
+        checked.value,
+        [0, 1], // valores possíveis
+        [COLOR, THEME.COLORS.GREY_100] // valores para quando for 0 ou 1
+      )
     }
   })
 
@@ -40,6 +58,10 @@ export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Prop
     scale.value = withTiming(1, { easing: Easing.bounce, duration: 300 }); // mudando a escala withTiming(modificador)
   }
 
+  useEffect(() => { // criando um useEffect para atualizar o valor de checked
+    checked.value = withTiming(isChecked ? 1 : 0, { duration: 100 });
+  }, [isChecked])
+
   return (
     <Pressable
       onPressIn={onPressIn}
@@ -50,17 +72,18 @@ export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Prop
         // Se eu utilizasse uma View normal do react native ela não ia aceitar as props de animção que eu fiz
         [
           styles.container,
+          { borderColor: COLOR },
           animatedContainerStyle, // utilizando o animatedStyle
-          { borderColor: COLOR, backgroundColor: isChecked ? COLOR : 'transparent' }
         ]
       }>
-        <Text style={
+        <Animated.Text style={
           [
             styles.title,
-            { color: isChecked ? THEME.COLORS.GREY_100 : COLOR }
-          ]}>
+            animatedTextStyle,
+          ]}
+        >
           {title}
-        </Text>
+        </Animated.Text>
       </Animated.View>
     </Pressable>
   );
