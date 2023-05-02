612bb0210cf357b9be3fd59a33d20d43ffffc536
Renato Ide Ogata <renatoogata95@gmail.com>
2023-05-02 20:35:12 -0300

    component Questio, keyframe animation

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,4 +1,5 @@
-import { View, Text } from 'react-native';
+import { Text, Dimensions } from 'react-native';
+import Animated, { Keyframe } from 'react-native-reanimated';
 
 import { Option } from '../Option';
 import { styles } from './styles';
@@ -14,9 +15,54 @@ type Props = {
   setAlternativeSelected?: (value: number) => void;
 }
 
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
 export function Question({ question, alternativeSelected, setAlternativeSelected }: Props) {
+
+  // Definir exatamente em um determinado frame oq fazer
+  const enteringKeyframe = new Keyframe({ // montando minha estilização animada
+    0: { // no frame 0
+      opacity: 0,
+      transform: [
+        { translateX: SCREEN_WIDTH }, // empurrando o componente para fora da tela
+        { rotate: '90deg' } // rotacionando o elemento
+      ]
+    },
+    70: { // no frame 70
+      opacity: 0.3,
+    },
+    100: { // no frame 100 (posição final)
+      opacity: 1,
+      transform: [
+        { translateX: 0 },
+        { rotate: '0deg' }
+      ]
+    }
+  })
+
+  // Forma mais resumida (começo e fim)
+  const exitingKeyframe = new Keyframe({
+    from: { // inicio
+      opacity: 1,
+      transform: [
+        { translateX: 0 },
+        { rotate: '0deg' }
+      ]
+    },
+    to: { // fim
+      opacity: 0,
+      transform: [
+        { translateX: SCREEN_WIDTH * (-1) }, // transformando em valor negativo para sair para a esquerda
+        { rotate: '-90deg' }
+      ]
+    },
+  })
   return (
-    <View style={styles.container}>
+    <Animated.View
+      style={styles.container}
+      entering={enteringKeyframe}
+      exiting={exitingKeyframe}
+    >
       <Text style={styles.title}>
         {question.title}
       </Text>
@@ -31,6 +77,6 @@ export function Question({ question, alternativeSelected, setAlternativeSelected
           />
         ))
       }
-    </View>
+    </Animated.View>
   );
 }
